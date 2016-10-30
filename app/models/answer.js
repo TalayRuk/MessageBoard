de5e8ba67fe82b9cbc4d1a368b8fee77d1eed330
vp <notejde@gmail.com>
2016-10-30 12:01:44 -0700

    add question & answer models

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  reply: DS.attr(),
+  image: DS.attr(),
+  questions: DS.belongTo('question', { async: true }),
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  topic: DS.attr(),
+  name: DS.attr(),
+  title: DS.attr(),
+  detail: DS.attr(),
+  answers: DS.hasMany('answer', { async: true})
 });
