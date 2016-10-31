3bad00a435fae2a6fab88c1ecbc7aac9fd240e2d
vp <notejde@gmail.com>
2016-10-31 03:22:32 -0700

    add answer-tile.js

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(answer) {
+      if (confirm('Are you sure you want to delte this answer?')) {
+        this.sendAction('deleteAnswer', answer);
+      }
+    }
+  }
 });
+//next need to add deleteAnswer action in question-detail.hbs to pass it up to the route handler (in this case routes/question.js)
