1b00b2e64653713981d95d437fd39b319e0c43d8
vp <notejde@gmail.com>
2016-10-31 02:58:56 -0700

    add new-answer.js

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+
+  actions: {
+    showAnswerform(){
+      this.set('addNewAnswer', true);
+    },
+    //save answer
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        reply: this.get('reply'),
+        image: this.get('image')
+      };
+      //reset addNewAnswer back to false;
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+      //send it up to route handler (index)
+    }
+  }
 });
