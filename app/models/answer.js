815037403ac5b0448929f551352e78f9d6f6ac14
vp <notejde@gmail.com>
2016-10-31 13:13:17 -0700

    add Problem: answer is not tide to questiongit add .

--- app/models/answer.js
+++ app/models/answer.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   author: DS.attr(),
   reply: DS.attr(),
   image: DS.attr(),
-  questions: DS.belongsTo('question', { async: true }),
+  questions: DS.belongsTo('question', { async: true })
 });
