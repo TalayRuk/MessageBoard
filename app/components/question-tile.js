0a7e9a29bafde83914006e71e123d917eb7a7e2d
vp <notejde@gmail.com>
2016-10-31 00:12:15 -0700

    add question-tile.js

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isDetailShowing: false,
+
+  actions: {
+    //button clicked
+    detailShow: function() {
+      this.set('isDetailShowing', true);
+    },
+    //hide the detailShow
+    detailHide: function() {
+      this.set('isDetailShowing', false);
+    }
+  }
 });
