7b52362a71e644b41b1c8ad9710be86aa72e5628
vp <notejde@gmail.com>
2016-10-31 02:12:27 -0700

    add updatequestion.js

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //start w/false so the button will show
+  updateQuestionForm: false;
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+      //when clicked form show
+    },
+    update(question) {
+      var params = {
+        topic: this.get('topic'),
+        name: this.get('name'),
+        title: this.get('title'),
+        detail: this.get('detail'),
+      };
+      //once save button is clicked set back to false;
+      this.set('updateQuestionForm', false);
+
+      this.sendAction('update', question, params);
+      //also send all the arguments through question-tile.js once get action from update-question.js b/c question-tile.hbs is a parent of update-question.hbs.
+    }
+  }
+
+
 });
