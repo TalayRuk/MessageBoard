cef9f9c0e2f523e6aefb42324e52bdfab92f8141
vp <notejde@gmail.com>
2016-10-31 01:49:13 -0700

    add new-question.js & update index.hbs for new question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //start with no form if questionFormShow btn clicked .. show form
+  addNewQuestion: false,
+  action: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    //add save
+    saveQuestion1() {
+      var params = {
+        topic: this.get('topic'),
+        name: this.get('name'),
+        title: this.get('title'),
+        detail: this.get('detail'),
+      };
+      //after each value is collected, form is hidden again
+      this.set('addNewQuestion', false);
+
+      this.sendAction('saveQuestion2', params);
+      //saveQuestion1 = the action that exist in new-rental;Job=to collects & save data & set them in a hash of keys w/the same name in firebase
+      //saveQuestion2 = a path =action up to route handler (in this case index.hbs)
+
+    }
+  }
 });
