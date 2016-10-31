fee6805e1f2360675fd0888960d9168e4a1ba03f
vp <notejde@gmail.com>
2016-10-31 01:54:35 -0700

    add index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
- //  model() {
- //   return Ember.RSVP.hash ({
- //     questions: this.store.findAll('question'),
- //     answers: this.store.findAll('answer')
- //   });
- // },
- // actions:
+  model() {
+    return Ember.RSVP.hash ({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
+  },
+  actions: {
+    saveQuestion3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+  }  
 });
