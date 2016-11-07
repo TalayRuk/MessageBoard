22c4411819c0504a52ad74db04dd4c7b2fb2c334
vp <notejde@gmail.com>
2016-11-07 01:54:49 -0800

    add action

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    addToCart(product) {
+      this.get('shoppingCart').add(product);
+    }
+  }
 });
