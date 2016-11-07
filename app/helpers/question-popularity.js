c0df8b4368f78a16c144b4b486c7de499134e73c
vp <notejde@gmail.com>
2016-11-06 23:40:24 -0800

    add question popularity

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function questionPopularity(params/*, hash*/) {
+  var question = params[0];
+
+  if (question.get('answers').get('length') >= 3) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star"></span>')
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);
---
+++ tests/unit/helpers/question-popularity-test.js
@@ -0,0 +1,12 @@
+
+import { questionPopularity } from 'message-board/helpers/question-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionPopularity([42]);
+  assert.ok(result);
+});
+
