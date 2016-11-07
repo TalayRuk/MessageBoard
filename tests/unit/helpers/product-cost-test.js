04c755f2928ee3ab23c3c882c432a76a3ed616eb
vp <notejde@gmail.com>
2016-11-07 03:50:33 -0800

    add product cost helper

---
+++ tests/unit/helpers/product-cost-test.js
@@ -0,0 +1,12 @@
+
+import { productCost } from 'message-board/helpers/product-cost';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | product cost');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = productCost([42]);
+  assert.ok(result);
+});
+
