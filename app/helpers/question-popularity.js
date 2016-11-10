16667483187ac6343bc44f647ee03f6403af9807
vp <notejde@gmail.com>
2016-11-09 23:40:59 -0800

    add master uptodate w/note branch

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -1,21 +1,5 @@
 import Ember from 'ember';
 
-<<<<<<< HEAD
-const {
-  Helper,
-  String
-} = Ember;
-
-export function questionPopularity(params/*, hash*/) {
-  var question = param[0];
-
-  if(question.get('answers').get('length' >= 3)) {
-    return String.htmlSafe('<span class="glyphicon glyphicon-star"></span>');
-  }
-}
-
-export default Helper.helper(questionPopularity);
-=======
 export function questionPopularity(params/*, hash*/) {
   var question = params[0];
 
@@ -25,4 +9,3 @@ export function questionPopularity(params/*, hash*/) {
 }
 
 export default Ember.Helper.helper(questionPopularity);
->>>>>>> note
